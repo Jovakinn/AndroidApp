db221d2c458f682fcf646be0dfba49afe7143e00
38098 <max05012004@gmail.com>
2020-04-11 22:51:22 +0200

    Initial commit

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private EditText password;
-    private Button button, button_alert;
+    private Button button, button_alert, button2;
     private RatingBar rating;
     private TextView text_show;
 
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         button_alert = (Button) findViewById(R.id.alert);
+        button2 = (Button) findViewById(R.id.button2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
                         MainActivity.this, password.getText(),
                         Toast.LENGTH_LONG
                 ).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                button2.setText("oops)");
+                button2.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
             }
         });
 
